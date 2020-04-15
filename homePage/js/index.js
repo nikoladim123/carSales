b35e2e88bec6a64aeb050584834c9d8b2c4f0670
unknown <nikoladimitrijevic1991@gmail.com>
2020-04-15 20:36:27 +0200

    car sales

--- homePage/js/index.js
+++ homePage/js/index.js
@@ -10,7 +10,7 @@ var banerAnim = document.getElementById('banerAnim');
 
 // headerCarousel[0].style.left = (carrouselBox.length-1) * -100 + 'vw';
 
-var colors = ['#ff2627', '#ffc02b','#a19fa4','#e69c3d', '#c90618', '#a54288', '#396fe3', '#be0616', '#c04825' ,'#d52e60','#6700ff', '#49113d'];
+var colors = ['#ff2627', '#ffc02b','#a19fa4','#e69c3d', '#c90618', '#b42994', '#396fe3', '#be0616', '#c04825' ,'#d52e60','#6700ff', '#49113d'];
 var carPosition = (carrouselBox.length-1) * -100;
 function moveCarousel() {
   if(carPosition < 0 ){
